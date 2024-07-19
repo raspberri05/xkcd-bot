92f8336898153178b8346c1ba28cc5be0a51bf22
Vedant Singhania <vedant.singhania@gmail.com>
2024-07-19 14:20:01 -0700

    finish random and latest

--- src/lib/scrape.py
+++ src/lib/scrape.py
@@ -1,2 +1,51 @@
-def get_latest_num():
-    return "hello"
+from bs4 import BeautifulSoup
+import requests
+import xml.etree.ElementTree as ET
+import random
+
+
+def get_curr_num():
+    url = "https://xkcd.com/rss.xml"
+    response = requests.get(url)
+    response.raise_for_status()
+    root = ET.fromstring(response.content)
+    curr_num = 0
+    for item in root.findall(".//channel/item"):
+        link = item.find("link")
+        curr_num = link.text.split("https://xkcd.com/")[-1].split("/")[0]
+        break
+    return int(curr_num)
+
+
+def get_random_num():
+    return random.randint(1, get_curr_num())
+
+
+def get_random():
+    num = get_random_num()
+    link = "https://xkcd.com/" + str(num)
+    return scrape(link)
+
+def get_latest():
+    num = get_curr_num()
+    link = "https://xkcd.com/" + str(num)
+    return scrape(link)
+
+
+def scrape(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.content, "html.parser")
+    
+    comic_div = soup.find("div", id="comic")
+    img_tag = comic_div.find("img") if comic_div else None
+    
+    title_div = soup.find("div", id="ctitle")  # Correctly finds the div with id 'ctitle'
+    title_text = title_div.get_text() if title_div else "No title found"  # Extract text if found, else default message
+    
+    img_title = img_tag.get("title") if img_tag else "No image title found"  # Get the title attribute from the img tag
+    
+    if img_tag:
+        return ["https:" + img_tag["src"], url, title_text, img_title]  # Include img_title as the fourth item
+    else:
+        return None
--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import os
 from discord import Intents
 from dotenv import load_dotenv
 from discord.ext import commands
-from lib.scrape import get_latest_num
+from discord import Embed
+from lib.scrape import get_random, get_latest
 
 load_dotenv()
 
@@ -16,7 +17,12 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.hybrid_command()
 async def latest(ctx):
-    await ctx.send("fetching latest xkcd comic...")
+    link = get_latest()
+    embed = Embed(title=link[2], url=link[1])
+    embed.set_image(url=link[0])  # Set the comic image as the embed image
+    embed.set_footer(text=link[3])
+    await ctx.send(embed=embed)
+
 
 
 @bot.hybrid_command()
@@ -26,7 +32,14 @@ async def get(ctx, number):
 
 @bot.hybrid_command()
 async def random(ctx):
-    await ctx.send("fetching random xkcd comic...")
+    try:
+        link = get_random()
+        embed = Embed(title=link[2], url=link[1])
+        embed.set_image(url=link[0])  # Set the comic image as the embed image
+        embed.set_footer(text=link[3])
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.send("failed to retrieve random xkcd comic. please contact the developer if this issue persists")
 
 
 @bot.event
